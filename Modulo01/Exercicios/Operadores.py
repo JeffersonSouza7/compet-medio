c19bf14bac525d23d27be2dbf5c62a2f376861dc
Jefferson Souza <jeffsouza1127@gmail.com>
2025-08-25 22:35:18 -0300

    Exercicio Operadores

---
+++ Modulo01/Exercicios/Operadores.py
@@ -0,0 +1,38 @@
+# Operadores Aritméticos
+
+# Exemplos
+
+# soma = 5 + 4
+# subtracao = 5 - 4
+# multiplicacao = 5 * 4
+# divisao = 5/4
+# divisao_parte_inteira = 5//4
+# divisao_resto = 5%4
+
+# Exercícios
+
+# 1 - Calcule a área de um retângulo (base*altura).
+
+base = 15
+altura = 30
+
+area = base * altura
+
+print(f'A area de um retangulo de base {base} e altural {altura} e igual a: {area}m2')
+
+# 2 - Converta minutos em horas inteiras e minutos restantes (use // e %).
+
+minutos = 135
+hora = 60
+
+print(f'O valor de {minutos} minutos e igual a: {minutos // hora} horas e {minutos % hora} minutos')
+
+# 3 - Dado um valor, calcule o quadrado e o cubo.
+
+valor = 7
+
+valorAoQuadrado = valor * valor
+valorAoCubo = valor * valor * valor
+
+print(f'O numero {valor} ao elevado ao quadrado e igual a: {valorAoQuadrado}')
+print(f'O numero {valor} ao elevado ao cubo e igual a: {valorAoCubo}')
