790f4c2c81adebd378f4656981766d59ef471a4e
Jefferson Souza <jeffsouza1127@gmail.com>
2025-08-25 22:11:31 -0300

    Primeiro exercicio

---
+++ Modulo01/Exercicios/Introducao.py
@@ -0,0 +1,22 @@
+# Introdução ao Python
+
+# Exercicios
+
+# 1 - Imprima seu nome e sua cidade.
+# 2 - Mostre o resultado de 2 + 3 * 4.
+# 3 - Escreva comentários explicando o que cada linha faz.
+
+
+# 1 - Imprima seu nome e sua cidade.
+
+print('Eu sou Jefferson, e sou do Recife-PE') # Exibindo uma mensagem no console
+
+# 2 - Mostre o resultado de 2 + 3 * 4.
+
+a = 2 # Definindo a variavel "a" como '2'
+b = 3 # Definindo a variavel "b" como '3'
+c = 4 # Definindo a variavel "c" como '4'
+
+calculo = a + b * c # Calculando a expressão (2 + 3 * 4)
+
+print(f'A soma de  (2 + 3 * 4) e igual a: {calculo}') # Exibindo o valor do calculo da expressão
