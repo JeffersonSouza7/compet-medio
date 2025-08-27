8cf1cbe23ab532ec8d6559660b4e8d99153fd41c
Jefferson Souza <jeffsouza1127@gmail.com>
2025-08-26 22:09:59 -0300

    Exercicio Variaveis, Tipos e Constantes

---
+++ Modulo01/Exercicios/VariaveisTiposConstantes.py
@@ -0,0 +1,12 @@
+# Variaveis, Tipos e constantes
+
+# 1 - Crie 3 variaveis para armazenar: nome{String}, idade(Inteiro), altura(Float) e exiba todos no final.
+
+nome = 'Jeff' #String
+idade = 27 #int (Inteiro)
+altura = 1.73 #float (Ponto flutuante)
+
+PI = 3.14159265 # Constante
+
+print(f'Ola, me chamo {nome}, tenho {idade} e minha altura é {altura}')
+print(f'{PI}')
