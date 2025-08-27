6454f39c9c306697186ab48af26651aff7dd8c13
Jefferson Souza <jeffsouza1127@gmail.com>
2025-08-26 22:19:13 -0300

    Exercicio Entrada e Saida de dados

---
+++ Modulo01/Exercicios/Entrada-SaidaDeDados.py
@@ -0,0 +1,17 @@
+# Entrada e saida de dados
+
+# 1 - Leia 2 numeros e mostre a soma deles
+
+n1 = int(input('Digite o 1º valor: '))
+n2 = int(input('Digite o 2º valor: '))
+
+soma = (n1 + n2)
+
+print(f'A soma entre {n1} e {n2} é igual a: {soma}')
+
+# 2 - Leia um nome e uma idade e imprima: "Fulano tem X anos"
+
+nomePessoa = input('Digite o seu nome: ')
+idadePessoa = input('Digite a sua idade: ')
+
+print(f'Olá {nomePessoa}, você tem {idadePessoa} anos, legal :D')
