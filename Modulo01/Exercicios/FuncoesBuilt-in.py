c100f646ec1f52565c926d10e571bbbb9b7b3df9
Jefferson Souza <jeffsouza1127@gmail.com>
2025-08-26 23:15:31 -0300

    Resolvendo mais exercicios propostos

--- Modulo01/Exercicios/FuncoesBuilt-in.py
+++ Modulo01/Exercicios/FuncoesBuilt-in.py
@@ -21,6 +21,35 @@ print(f'O maior numero digitado foi: {max(numeros)}')
 print(f'O menor numero digitado foi: {min(numeros)}')
 print(f'A soma dos 3 numero e igual a: {sum(numeros)}')
 
+# 2 - Peça ao usuário para digitar uma palavra e:
+# Mostre a palavra em maiúsculas (função upper()).
+# Mostre a palavra em minúsculas (função lower()).
+# Mostre quantas letras a palavra tem (função len()).
+
+palavra = input('Digite uma palavra qualquer: ')
+
+print(f'A palavra em maiusculas é:', palavra.upper())
+print(f'A palavra em minusculas é:', palavra.lower())
+print(f'A palavra tem um total de:', len(palavra),'letras')
 
 
 
+# 3 - Leia 3 números em uma lista e exiba max, min e sum.
+
+listaNumeros = [3, 5, 7]
+
+print('O maior, o menor e a soma de todos os 3 numeros são respectivamente:', max(listaNumeros),',', min(listaNumeros), 'e', sum(listaNumeros))
+
+# 4 - Arredonde 7/3 com 2 casas.
+
+divisao = (7/3)
+
+print(round(divisao, 2))
+
+# 5 - Transforme "42" em int e "3.5" em float.
+
+valorInt = "42"
+valorFloat = "3.5"
+
+print(f'A variavel "valorInt" agora é do tipo ', type(int(valorInt)))
+print(f'A variavel "valorFloat" agora é do tipo ', type(float(valorFloat)))
